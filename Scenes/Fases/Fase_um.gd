6a48da65904eac2881683483cdb6ebcee1ca2caf
Ravensherald <dinodroid3000@gmail.com>
2021-02-01 02:00:21 -0300

    Progamation Operation das fala espero que não quebre nada por favor git faz essa pa nois

--- Scenes/Fases/Fase_um.gd
+++ Scenes/Fases/Fase_um.gd
@@ -1,6 +1,13 @@
 extends Node
-var conversando = false
+var conversando = "nenhuma"
 var conversa_intro = 0
+var conversa_submarino = 0
+var conversa_porta_um = 0
+
+var conversa_porta_um_aberta = 0
+var conversa_morte = 0
+var conversa_lab = 0
+var conversa_final = 0
 
 func _ready():
 	
@@ -10,26 +17,107 @@ func _ready():
 
 func _process(delta):
 	
-	if conversando and Input.is_action_just_pressed("Interagir"):
-		intro_conversation()
-		
+	if conversando == "submarino" and Input.is_action_just_pressed("Interagir"):
+		submarino_conversation()
+	if conversando == "porta_um_aberta" and Input.is_action_just_pressed("Interagir"):
+		porta_um_aberta_conversation()
 	
-
 
 func _on_Area2D_body_entered(body):
 	
-	intro_conversation()
+	submarino_conversation()
 	
 	
 
 
 func intro_conversation():
+	
 	if conversa_intro <= 13:
-		CCR_Global.cutscene = true
-		conversando = true
+		
+		conversando = "intro"
+		
 		CCR_Global.conversando("intro", conversa_intro)
 		conversa_intro += 1
+		
 	elif conversa_intro > 13:
-		conversando = false
-		CCR_Global.cutscene = false
+		conversando = "nenhuma"
+		
+	
+	
+func submarino_conversation():
+	
+	if conversa_submarino <= 4:
+		
+		conversando = "submarino"
+		
+		CCR_Global.conversando("submarino", conversa_submarino)
+		conversa_submarino += 1
+		
+	elif conversa_submarino > 3:
+		conversando = "nenhuma"
+		
+	
+	
+
+
+
+
+		
+		
+		
+func porta_um_aberta_conversation():
+	
+	if conversa_porta_um_aberta <= 2:
+		
+		conversando = "porta_um_aberta"
+		
+		CCR_Global.conversando("porta_um_aberta", conversa_porta_um_aberta)
+		conversa_porta_um_aberta += 1
+		
+	elif conversa_porta_um_aberta > 1:
+		conversando = "nenhuma"
+		
+
+func morte_conversation():
+	
+	if conversa_morte <= 5:
+		
+		conversando = "morte"
+		
+		CCR_Global.conversando("morte", conversa_morte)
+		conversa_morte += 1
+		
+	elif conversa_morte > 5:
+		conversando = "nenhuma"
+		
+		
+func lab_conversation():
+	
+	if conversa_lab <= 5:
+		
+		conversando = "lab"
+		
+		CCR_Global.conversando("lab", conversa_lab)
+		conversa_lab += 1
+		
+	elif conversa_lab > 5:
+		conversando = "nenhuma"
+		
+		
+func final_conversation():
+	
+	if conversa_final <= 14:
+		
+		conversando = "final"
+		
+		CCR_Global.conversando("final", conversa_final)
+		conversa_final += 1
+		
+	elif conversa_final > 14:
+		conversando = "nenhuma"
+		
+
+
+func _on_porta_um_abrindo_body_entered(body):
+	porta_um_aberta_conversation()
 	
